27d337d1905a740fd914009775ee2de87932967c
Wandile Ndlovu <wandilen158@gmail.com>
2025-04-01 17:29:12 +0200

    initial commit

---
+++ index.py
@@ -0,0 +1,17 @@
+prompt = input("Enter the file name you wish to open: ")
+
+data = ""
+
+try:
+    with open(prompt, "r") as file:
+        data = file.read()
+except FileNotFoundError:
+    print("File not found. Please check the file Name.")
+else:
+    modifyData = data.upper()
+    modify_txt= "modified_" + prompt 
+
+with open("word.txt", "w") as file:
+    file.write(modifyData)
+    print(data)
+
